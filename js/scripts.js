09d4cacf1b575274af919ddd1cd049929f768064
Michael Korir <mikor22@gmail.com>
2019-03-06 15:25:23 +0300

    add prototype for employer

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function Employer (jobType, duration, amount, phoneNumber, email, idNumber, description){
+  this.jobType= job;
+  this.duration= duration;
+  this.amount= amount;
+  this.phoneNUmber= number;
+  this.email= email;
+  this.idNumber= idNumber;
+  this.description= description;
+};
